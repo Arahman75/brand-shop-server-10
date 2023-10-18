ab84f92c69191b89a742b52da741db4ce6f64bc0
Arahman75 <selfarahman01825@gmail.com>
2023-10-18 22:40:28 +0600

    added the product post api

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# b8a10-brandshop-server-side-Arahman75" 
+https://github.com/programming-hero-web-course-4/b8a10-brandshop-server-side-Arahman75
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const cors = require('cors')
+require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
@@ -8,10 +10,64 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+// automotiveBrand
+// AwmJGwlmoMDD3nRp
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ik9fyhp.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const brandCollection = client.db('automotiveDB').collection('brands');
+        const productCollection = client.db('automotiveDB').collection('products');
+
+        // get the brand
+        app.get('/brands', async (req, res) => {
+            const cursor = brandCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // post product
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Brand server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Brand server is running port ${port}`)
 })
