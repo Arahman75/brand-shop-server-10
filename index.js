88df61bcc0403ee22f10d78bf1cde2aee7e10e1e
Arahman75 <selfarahman01825@gmail.com>
2023-10-20 08:51:34 +0600

    added the updete api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
@@ -52,6 +52,39 @@ async function run() {
         app.get('/products', async (req, res) => {
             const cursor = productCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('update id', id);
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateProduct = req.body;
+            const product = {
+                $set: {
+                    name: updateProduct.name,
+                    brandName: updateProduct.brandName,
+                    price: updateProduct.price,
+                    description: updateProduct.description,
+                    rating: updateProduct.rating, image: updateProduct.image
+                }
+            }
+            const result = await productCollection.updateOne(filter, product, options);
             res.send(result)
         })
 
