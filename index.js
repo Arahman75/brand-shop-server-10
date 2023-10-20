1acc190adfefd885a75301c3e3360afdbf53967a
Arahman75 <selfarahman01825@gmail.com>
2023-10-20 19:44:58 +0600

    change the some code

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ async function run() {
             res.send(result)
         })
 
+
         app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
